6920422de2eeef0b47a8263fce769f14ca3cb50d
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-09-26 23:24:14 +0300

    Добавил метод top_10_nodes_by_speedd который возвращает 10 узлов с самой высокой средней скоростью передачи данных

--- parser.py
+++ parser.py
@@ -5,6 +5,7 @@ class TrafficParser():
         self.unique_nodes = set()
         self.average_udp_speed = 0
         self.average_tcp_speed = 0
+        self.node_speeds = {} # Словарь для хранения скоростей передачи данных для каждого узла
 
         with open("traf.txt", "r") as traffic_dump:
             for line in traffic_dump:
@@ -21,6 +22,17 @@ class TrafficParser():
             source_ip_port, dest_ip_port = fields[0], fields[2]
             self.unique_nodes.add(source_ip_port)
             self.unique_nodes.add(dest_ip_port)
+
+            # Обновляем скорость передачи данных для каждого узла в словаре
+            if source_ip_port in self.node_speeds:
+                self.node_speeds[source_ip_port] += data_size / time
+            else:
+                self.node_speeds[source_ip_port] = data_size / time
+
+            if dest_ip_port in self.node_speeds:
+                self.node_speeds[dest_ip_port] += data_size / time
+            else:
+                self.node_speeds[dest_ip_port] = data_size / time
 
             self.total_data_size += data_size
             self.total_time += time
@@ -44,12 +56,20 @@ class TrafficParser():
         else:
             return "Нет, UDP не используется для передачи данных с максимальной пиковой скоростью."
 
+    def top_10_nodes_by_speed(self):
+        """Возвращает 10 узлов с самой высокой средней скоростью передачи данных (байт/сек)"""
+        sorted_nodes = sorted(self.node_speeds.items(), key=lambda x: x[1], reverse=True)
+        top_10 = sorted_nodes[:10]
+        return top_10
+
 
 parser = TrafficParser()
 unique_nodes_result = parser.unique_nodes_in_the_network()
 average_data_rate = parser.average_data_rate()
 udp_tcp_speed = parser.compare_udp_tcp_speed()
+top_10_nodes = parser.top_10_nodes_by_speed()
 
-print(f"_______________________________________________________________________________________\n{unique_nodes_result}")
-print(f"_______________________________________________________________________________________\n{average_data_rate}")
-print(f"_______________________________________________________________________________________\n{udp_tcp_speed}")
+print(f"_______________________________________________________________________________________\nQ1: {unique_nodes_result}")
+print(f"_______________________________________________________________________________________\nQ2: {average_data_rate}")
+print(f"_______________________________________________________________________________________\nQ3: {udp_tcp_speed}")
+print(f"_______________________________________________________________________________________\nQ4: Топ 10 узлов по скорости передачи данных: {top_10_nodes}")
