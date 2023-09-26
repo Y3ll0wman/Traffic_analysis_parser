a0f91ab9ebb3b4178120b2f2a2bbbc4363639599
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-09-27 00:56:38 +0300

    Добавил метод top_10_subnets_by_sessions который возвращает 10 самых активных подсетей /24

--- parser.py
+++ parser.py
@@ -1,27 +1,39 @@
+import ipaddress
+
+
 class TrafficParser():
     def __init__(self):
-        self.total_data_size = 0
+        self.total_data_size = 0  # Общий размер переданных данных в байтах
         self.total_time = 0
         self.unique_nodes = set()
-        self.average_udp_speed = 0
-        self.average_tcp_speed = 0
-        self.node_speeds = {} # Словарь для хранения скоростей передачи данных для каждого узла
+        self.average_udp_speed = 0  # Средняя скорость для UDP
+        self.average_tcp_speed = 0  # Средняя скорость для TCP
+        self.node_speeds = {}  # Словарь для хранения скоростей передачи данных для каждого узла
+        self.subnet_sessions = {}  # Словарь для хранения количества сессий для каждой подсети /24
 
         with open("traf.txt", "r") as traffic_dump:
+            # Читаем каждую строку из файла
             for line in traffic_dump:
+                # Разбиваем строку на поля, используя символ ';'
                 fields = line.strip().split(";")
                 is_udp = fields[4]
+
+                # Извлекаем размер переданных данных и время передачи
                 data_size = int(fields[5])
                 time = float(fields[6])
 
+            # Извлекаем IP адреса и MAC адреса отправителя и получателя
+            source_ip_port, dest_ip_port = fields[0], fields[2]
+
+            # Добавляем IP адреса и MAC адреса отправителя и получателя в множество
+            self.unique_nodes.add(source_ip_port)
+            self.unique_nodes.add(dest_ip_port)
+
+            # Вычисляем общий размер трафика и время для UDP и TCP соединений
             if is_udp == 'true':
                 self.average_udp_speed += data_size / time
             else:
                 self.average_tcp_speed += data_size / time
-
-            source_ip_port, dest_ip_port = fields[0], fields[2]
-            self.unique_nodes.add(source_ip_port)
-            self.unique_nodes.add(dest_ip_port)
 
             # Обновляем скорость передачи данных для каждого узла в словаре
             if source_ip_port in self.node_speeds:
@@ -29,11 +41,26 @@ class TrafficParser():
             else:
                 self.node_speeds[source_ip_port] = data_size / time
 
+            # Если узел уже существует в словаре, добавляем скорость передачи данных, иначе создаем новую запись
             if dest_ip_port in self.node_speeds:
                 self.node_speeds[dest_ip_port] += data_size / time
             else:
                 self.node_speeds[dest_ip_port] = data_size / time
 
+            # Анализируем IP-адреса и сессии подсетей
+            source_ip = source_ip_port.split(":")[0]
+            dest_ip = dest_ip_port.split(":")[0]
+
+            # Извлекаем подсеть /24 из IP-адресов и преобразуем их в строку
+            # для дальнейшего анализа и отслеживания сессий по подсетям
+            source_subnet = str(ipaddress.IPv4Network(source_ip, strict=False).supernet(prefixlen_diff=8))
+            dest_subnet = str(ipaddress.IPv4Network(dest_ip, strict=False).supernet(prefixlen_diff=8))
+
+            # Обновляем количество сессий для каждой подсети в словаре
+            self.subnet_sessions[source_subnet] = self.subnet_sessions.get(source_subnet, 0) + 1
+            self.subnet_sessions[dest_subnet] = self.subnet_sessions.get(dest_subnet, 0) + 1
+
+            # Увеличиваем общий размер и общее время
             self.total_data_size += data_size
             self.total_time += time
 
@@ -62,14 +89,22 @@ class TrafficParser():
         top_10 = sorted_nodes[:10]
         return top_10
 
+    def top_10_subnets_by_sessions(self):
+        """Возвращает 10 самых активных подсетей /24 (A.B.C.xxx) по количеству сессий передачи данных (штук)"""
+        sorted_subnets = sorted(self.subnet_sessions.items(), key=lambda x: x[1], reverse=True)
+        top_10 = sorted_subnets[:10]
+        return top_10
+
 
 parser = TrafficParser()
 unique_nodes_result = parser.unique_nodes_in_the_network()
 average_data_rate = parser.average_data_rate()
 udp_tcp_speed = parser.compare_udp_tcp_speed()
 top_10_nodes = parser.top_10_nodes_by_speed()
+top_10_subnets = parser.top_10_subnets_by_sessions()
 
 print(f"_______________________________________________________________________________________\nQ1: {unique_nodes_result}")
 print(f"_______________________________________________________________________________________\nQ2: {average_data_rate}")
 print(f"_______________________________________________________________________________________\nQ3: {udp_tcp_speed}")
 print(f"_______________________________________________________________________________________\nQ4: Топ 10 узлов по скорости передачи данных: {top_10_nodes}")
+print(f"_______________________________________________________________________________________\nQ5: Топ 10 подсетей по количеству сессий:\n{top_10_subnets}")
